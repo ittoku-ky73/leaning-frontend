0bf13e39c648a8d5d221d7bbbfd845feb2c184c6
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-08-27 18:03:51 +0900

    finished Image gallery

--- js/Building_blocks/image_gallery/main.js
+++ js/Building_blocks/image_gallery/main.js
@@ -1,18 +1,53 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
 /* Declaring the alternative text for each image file */
+const images = [
+  {
+    src: 'images/pic1.jpg',
+    alt: 'child eye',
+  },
+  {
+    src: 'images/pic2.jpg',
+    alt: 'desert',
+  },
+  {
+    src: 'images/pic3.jpg',
+    alt: 'hydrangea',
+  },
+  {
+    src: 'images/pic4.jpg',
+    alt: 'egypt',
+  },
+  {
+    src: 'images/pic5.jpg',
+    alt: 'butterfly',
+  },
+];
 
 /* Looping through images */
+images.forEach(image => {
+  const newImage = new Image();
+  newImage.src = image.src;
+  newImage.alt = image.alt;
+  thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', image.src);
+  });
+});
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  if (overlay.classList.contains('dark')) {
+    btn.textContent = 'Lighten';
+    overlay.classList.replace('dark', 'light');
+  }
+  else if (overlay.classList.contains('light')) {
+    btn.textContent = 'Darken';
+    overlay.classList.replace('light', 'dark');
+  }
+});
