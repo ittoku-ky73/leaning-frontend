4a136700641f8af7869058b990e2d6b4890abe3c
いっとく <73953416+ittoku703@users.noreply.github.com>
2022-09-21 02:18:19 +0900

    Finish sequencing animation

--- js/Asynchronous/Sequencing_animation/script.js
+++ js/Asynchronous/Sequencing_animation/script.js
@@ -9,6 +9,38 @@ const aliceTiming = {
   fill: 'forwards'
 }
 
-const alice1 = document.querySelector("#alice1");
-const alice2 = document.querySelector("#alice2");
-const alice3 = document.querySelector("#alice3");
+const alices = document.querySelectorAll('#alice-container img');
+
+// // Callback Hell ver.
+// setTimeout(() => {
+//   alices[0].animate(aliceTumbling, aliceTiming);
+//   setTimeout(() => {
+//     alices[1].animate(aliceTumbling, aliceTiming);
+//     setTimeout(() => {
+//       alices[2].animate(aliceTumbling, aliceTiming);
+//     }, aliceTiming.duration)
+//   }, aliceTiming.duration)
+// }, 0)
+
+// // Arrow function ver.
+// alices[0].animate(aliceTumbling, aliceTiming).finished
+//   .then(() => alices[1].animate(aliceTumbling, aliceTiming).finished)
+//   .then(() => alices[2].animate(aliceTumbling, aliceTiming).finished)
+//   .catch((error) => console.error(`Error animating Alices: ${error}`));
+
+// async await ver.
+async function alicesRotating() {
+  try {
+    await alices[0].animate(aliceTumbling, aliceTiming).finished;
+    await alices[1].animate(aliceTumbling, aliceTiming).finished;
+    await alices[2].animate(aliceTumbling, aliceTiming).finished;
+  }
+  catch (error) {
+    console.error(`Error animating Alices: ${error}`);
+  }
+  finally {
+    console.log('Animation\'s promise is worked');
+  }
+}
+
+alicesRotating();
